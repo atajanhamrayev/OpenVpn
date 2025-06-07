ce2c49ec1def2db174f94f15bce87cea4482e84d
Atajan Hamrayev <116101193+atajanhamrayev@users.noreply.github.com>
2025-06-07 14:50:42 +0500

    Update OpenScript.sh

--- OpenScript.sh
+++ OpenScript.sh
@@ -1,9 +1,5 @@
 apt update && apt -y install ca-certificates wget net-tools gnupg
-
 wget https://as-repository.openvpn.net/as-repo-public.asc -qO /etc/apt/trusted.gpg.d/as-repository.asc
-
 echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/as-repository.asc] http://as-repository.openvpn.net/as/debian focal main" > /etc/apt/sources.list.d/openvpn-as-repo.list
-
 apt update && apt -y install openvpn-as=2.11.2-72c0e923-Ubuntu20
-
 apt-mark hold openvpn-as openvpn-as-bundled-clients
